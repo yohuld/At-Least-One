49f4326e070d08bfe6cacb4723f269b5e716958f
yohuld <133837602+yohuld@users.noreply.github.com>
2023-07-09 14:12:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function atLeastOne(){
 		let word, res = 100 * (1 - calc);
 		if (res > 99.99 && chance < 100){res -= 0.01;}
 		if (res > 7.99 && res < 8.99){word = "an";}
-			else if (res > 10.99 && res < 12){word = "an";}
-			else if (res > 79.99 && res < 90){word = "an";}
-			else {word = "a";}
+		else if (res > 10.99 && res < 12){word = "an";}
+		else if (res > 79.99 && res < 90){word = "an";}
+		else {word = "a";}
 		document.querySelector("textarea").innerHTML=`There is ${word} ${res.toFixed(2)}% chance of at least one success`;}
 	catch(err){
 		document.querySelector("textarea").innerHTML=err;
